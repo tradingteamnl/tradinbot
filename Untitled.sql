bcca63d1e4df369ee5f819349e4d40ddc9e53fb3
michel <micheldaniel2@hotmail.com>
2017-03-28 13:16:58 +0200

    sql dump
    sql dump

---
+++ Untitled.sql
@@ -0,0 +1,85 @@
+-- MySQL dump 10.13  Distrib 5.7.12, for osx10.9 (x86_64)
+--
+-- Host: 127.0.0.1    Database: cryptoData3_0
+-- ------------------------------------------------------
+-- Server version	5.7.16
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `bitstampmarktdata`
+--
+
+DROP TABLE IF EXISTS `bitstampmarktdata`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `bitstampmarktdata` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Markt` varchar(45) NOT NULL,
+  `High` double NOT NULL DEFAULT '-1',
+  `Low` double NOT NULL DEFAULT '-1',
+  `Bid` double NOT NULL DEFAULT '-1',
+  `Ask` double NOT NULL DEFAULT '-1',
+  `Datum` varchar(45) DEFAULT NULL,
+  `Time` varchar(45) DEFAULT NULL,
+  `VolumeBTC` double NOT NULL DEFAULT '-1',
+  `Last` double NOT NULL DEFAULT '-1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `datum`
+--
+
+DROP TABLE IF EXISTS `datum`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `datum` (
+  `Iddatum` int(11) NOT NULL AUTO_INCREMENT,
+  `Datum` varchar(10) NOT NULL,
+  `Gemiddeldeuitgerekend` int(1) DEFAULT '0',
+  PRIMARY KEY (`Iddatum`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `marktdata`
+--
+
+DROP TABLE IF EXISTS `marktdata`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `marktdata` (
+  `Handelsplaats` varchar(45) NOT NULL,
+  `Markt` varchar(45) NOT NULL,
+  `High` double NOT NULL,
+  `Low` double NOT NULL,
+  `Volume` double NOT NULL,
+  `VolumeBTC` double NOT NULL,
+  `Bid` double NOT NULL,
+  `Ask` double NOT NULL,
+  `Last` double NOT NULL,
+  PRIMARY KEY (`Handelsplaats`,`Markt`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-03-28 13:16:00
