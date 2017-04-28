6315d5d5cd8b5a1912c514b11c28fd6954955878
michel <micheldaniel2@hotmail.com>
2017-04-28 12:14:26 +0200

    add storer
    oder systeem add

--- src/drivers/Drivers.java
+++ src/drivers/Drivers.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package drivers;
 
 //import
@@ -11,43 +6,45 @@ import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 import org.json.JSONObject;
+import tradingbot.SetOrder;
 
 /**
  *
  * @author michel
  */
 public class Drivers {
-    
-    
+
     public static void main(String[] args) {
-        
+
         //great object filesystem
         FileSystem filesystem = new FileSystem();
-        
-        //terminalSpawn.command("lol");
-        
+        SetOrder setOrder = new SetOrder();
+
         //laat config bestand;
         String config = filesystem.readConfig();
-        
+
         //request url
         final String BITTREX_BASIS_URL = new JSONObject(config).getJSONObject("bittrex").getString("basisUrl");
-        
+
         //Great drivers
         BittrexDrivers bittrexdrivers = new BittrexDrivers(config, BITTREX_BASIS_URL);
         AnalytisDrivers analyticsDrivers = new AnalytisDrivers(config, BITTREX_BASIS_URL);
-        
+
+        //reload
         TimerTask task = new TimerTask() {
 
             @Override
             public void run() {
-               bittrexdrivers.start();
-               analyticsDrivers.start();
+                setOrder.orderSystem();
+                bittrexdrivers.start();
+                analyticsDrivers.start();
             }
         };
-        
+
         Timer timer = new Timer();
+
         timer.schedule(task, new Date(), 60000);
-        
+
         System.out.println("Reload code is gestart.");
     }
 }
