6fba2eb7eaebc3de144316c7e8aeb98f16eec6ea
michel <micheldaniel2@hotmail.com>
2017-05-03 12:07:53 +0200

    Mysql package update
    Mysql heb ik de sql van een update gegeven.

--- src/mysql/Mysqlsql.java
+++ src/mysql/Mysqlsql.java
@@ -19,6 +19,12 @@ public class MysqlSql {
     private final String PASSWORD = mysqlconnector.getPassword();
     private final String CONN_STRING = mysqlconnector.getUrlmysql();
 
+    /**
+     * 
+     * @param sqlString
+     * @return data uit het database
+     * @throws SQLException 
+     */
     public ResultSet mysqlSelectStament(String sqlString) throws SQLException {
 
         //connecntie mysql
@@ -28,6 +34,25 @@ public class MysqlSql {
 
         //return
         return stmt.executeQuery(sqlString);
+    }
+    
+    /**
+     * 
+     * @param sqlString
+     * @return of het gelukt is of niet 
+     */
+    public boolean mysqlStament(String sqlString){
+        Connection conn;
+        try {
+            conn = DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
+            Statement stmt = (Statement) conn.createStatement();
+            
+            stmt.executeQuery(sqlString);
+            
+            return true;
+        } catch (SQLException ex) {
+            return false;
+        }
     }
 
 }
