f8ad544dfd939d157db053b415ca62da95fba54d
michel <micheldaniel2@hotmail.com>
2017-05-12 00:35:30 +0200

    orderStatus
    order status systeem gemaakt.

--- src/core/OrderSysteem/OrderStatus.java
+++ src/core/OrderSysteem/OrderStatus.java
@@ -1,10 +1,17 @@
 package core.OrderSysteem;
 
+import bittrex.BittrexProtocall;
 import http.Http;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import mysql.MysqlSql;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+/**
+ *
+ * @author michel
+ */
 public class OrderStatus {
 
     //array list voor lijst order status
@@ -13,65 +20,122 @@ public class OrderStatus {
     //objecten
     MysqlSql mysqlSql = new MysqlSql();
     Http http = new Http();
-
+    BittrexProtocall bittrexProtocall = new BittrexProtocall();
+    OrderDBSql orderDBSql = new OrderDBSql();
+    
     //constructor
     public OrderStatus() {
         this.ORDERLIST = new int[]{1, 2, 3};
     }
+    
+    //methoden die orderStatus aanstuurt
+    public void run() {
 
-    public void orderList() {
-
-        //loop door alle prioriteiten heen
+        //loop diie de prio lijsten heen
         for (int i = 0; i < ORDERLIST.length; i++) {
 
-            //if loop
-            if (dbCheck(i) == true) {
-                try {
-                    //SQL string
-                    String getActiveOrders = "SELECT * FROM orderID WHERE status='active'";
+            int prio = ORDERLIST.length;
 
-                    //vraag active orders op
-                    ResultSet resultsetActiveOrder = mysqlSql.mysqlSelectStament(getActiveOrders);
+            //kijk of er iets van bittrex order staat
+            try {
+                ResultSet rs = mysqlSql.mysqlSelectStament("SELECT COUNT(*) AS total FROM orderID WHERE prioritijd='" + prio + "'");
+                while (rs.next()) {
+                    if (rs.getInt("total") != 0) {
 
-                    //loop
-                    while (resultsetActiveOrder.next()) {
-
+                        //call bittrex methoden
+                        try {
+                            bittrex();
+                        } catch (SQLException ex) {
+                            System.err.println("Probleem in de bittrex methoden in OrderStatus.");
+                        }
+                    } else {
+                        System.out.println("Er zijn geen open order. Met prioriteit "+ prio+".");
                     }
-                } catch (SQLException ex) {
-                    System.err.println("Er is een probleem in orderStatus.java");
+
                 }
+            } catch (SQLException ex) {
+                System.err.println("Probleem bij orderID gevens op te vragen bij markt bittrex");
             }
         }
     }
-
+    
     /**
-     *
-     * @param prio
-     * @return return of er regels beschikbaar zijn met die prio
+     * Deze methoden laat alles in voor bittrex
+     * @throws SQLException als er een fout optreed ergens onderweg
      */
-    private boolean dbCheck(int prio) {
+    private void bittrex() throws SQLException {
+        
+        //exchange
+        String exchange = "bittrex";
+        
+        //maak jsonObject
+        JSONObject object = new JSONObject(bittrexProtocall.getOpenOrders());
 
-        try {
-            //vraag active orders op
-            ResultSet resultsetActiveOrder = mysqlSql.mysqlSelectStament("SELECT COUNT(*) AS total FROM orderID WHERE status='active' AND prioritijd='" + prio + "'");
-            while (resultsetActiveOrder.next()) {
-                if (resultsetActiveOrder.getInt("total") == 0) {
-                    //Er zijn geen orders
-                    System.out.println("Er zijn geen active order met prioritijd " + prio + ".");
-                    return false;
-                } else {
-                    //Er zijn orders die open staan
-                    System.out.println("Er zijn orders die open staan.");
-                    return true;
+        if ("true".equals(object.getJSONObject("result").getString("succes"))) {
+
+            //maak json array
+            JSONArray array = new JSONArray(object.getJSONArray("result"));
+
+            //loop
+            for (int i = 0; i < array.length(); i++) {
+
+                //order
+                JSONObject order = new JSONObject(array.getJSONObject(i));
+
+                //krijg alle data er uit
+                String uuid = order.getString("OrderUuid");
+                String markt = order.getString("markt");
+                double total = order.getDouble("Quantity");
+                double remaning = order.getDouble("QuantityRemaining");
+                double prijs = order.getDouble("price");
+
+                //kijk of de order is aangepast
+                String sqlSelect1 = "SELECT COUNT(*) AS total FROM orderID"
+                        +" WHERE uuid='" + uuid + "' AND total='" + total + " AND remaning='" + remaning + "' AND exchange='bittrex' AND status='pending'";
+                ResultSet rs = mysqlSql.mysqlSelectStament(sqlSelect1);
+
+                //kijk of het 0 of 1 is
+                int count;
+                while (rs.next()) {
+                    count = rs.getInt("total");
+                    if (count == 1) {
+                        System.out.println("Order "+uuid +" op Bittrex is nog up to date.");
+                        
+                        //update timestamp
+                        orderDBSql.updateTimeStamp(uuid, exchange);
+                    } else if(count == 0){
+                        
+                        //hier word gekeken of de order in de lijst staat en zo ja dan worden database velden bijgewerkt
+                        String sqlSelect2 = "SELECT COUNT(*) AS total FROM orderID WHERE uuid='" + uuid + "' AND exchange='bittrex' AND status='pending'";
+                        ResultSet rs1 = mysqlSql.mysqlSelectStament(sqlSelect2);
+                        
+                        int count1;
+                        while (rs1.next()){
+                            count1 = rs1.getInt("total");
+                            
+                            //als het 0 is
+                            if(count1 == 0){
+                                
+                                //voeg order to in het database
+                                orderDBSql.bittrexAddOrder(uuid, markt, total, remaning, prijs, i);
+                                
+                            } else if(count1 == 1){
+                                
+                                //update order
+                                orderDBSql.bittrexUpdateOrder(uuid, remaning);
+                                
+                            } else{
+                                System.err.println("Er us een probleem bij OrderStatus. De uuid nummer van bittrex komt meedere keren voor in het database.");
+                            }
+                        }
+                        
+                    } else {
+                        System.err.println("Er is een bug in de if loop (core/OrderSysteem/OrderStatus.java).");
+                    }
                 }
             }
-
-            //als er helemaal geen andere optie zijn (kut netbeans)
-            return false;
-        } catch (SQLException ex) {
-            System.err.println("Er is een probleem in DBCheck.");
-            return false;
+        } else {
+            System.err.println("Er is een error bij open order op te vragen bij bittrex.");
         }
     }
-
 }
--- src/core/OrderSysteem/orderDBSql.java
+++ src/core/OrderSysteem/orderDBSql.java
@@ -1,36 +1,109 @@
 package core.OrderSysteem;
 
+import global.Time;
 import mysql.MysqlSql;
 
 /**
  *
  * @author michel
  */
-public class orderDBSql {
-    
+public class OrderDBSql {
+
     //maak object aan
     MysqlSql mysqlSql = new MysqlSql();
-    
-    
-    public void bittrexAddOrder(String uuid, String markt, double totaal, double remaning, double prijs, int prio){
+    Time timeModule = new Time();
+
+    /**
+     *
+     * @param orderID unieke code
+     * @param markt bijvoorbeeld BTC-LTC
+     * @param totaal hoeveel je er aan koopt
+     * @param remaning hoeveel je er nog moet doen
+     * @param prijs de prijs
+     * @param prio de snelheid
+     */
+    public void bittrexAddOrder(String orderID, String markt, double totaal, double remaning, double prijs, int prio) {
+
+        //variable
         String status;
-        
+        int timeStamp = timeModule.getTimeStamp();
+
         //om de juiste status in het db te zetten
-        if(remaning !=0){
+        if (remaning != 0) {
             status = "pending";
         } else {
             status = "compleet";
         }
-        
-        //sql String
-        String sql = "INSERT INTO orderID (markt, orderID, totaal, remaning, prijs, exchange, prioritijd, status)" +
-            "VALUES ('"+markt+"', '"+uuid+"', '"+totaal+"', '"+remaning+"', '"+prijs+"', 'bittrex', '"+prio+"', );";
-        
-        //Stuur de sql sting naar het database
-        mysqlSql.mysqlStament(sql);
+
+        //kijk of het goed is gegaan
+        String addSql = "INSERT INTO orderID (markt, orderID, totaal, remaning, prijs, exchange, prioritijd, status, updateTime)"
+                + "VALUES ('" + markt + "', '" + orderID + "', '" + totaal + "', '" + remaning + "', '" + prijs + "', 'bittrex', '" + prio + "', '" + status + "', " + timeStamp + "');";
+        boolean mysqlResponse = mysqlSql.mysqlStament(addSql);
+
+        //kijk op het goed is gegaan
+        if (mysqlResponse == true) {
+            System.out.println("Order " + orderID + " op exchange is de timestamp bijgewerkt.");
+        } else {
+            System.err.println("Er is een error bij timestamp updaten bij bittrex. Order id is " + orderID + ".");
+        }
     }
-    
-    public void bittrexUpdateOrder(){
-        
+
+    /**
+     *
+     * @param orderID unieke order id
+     * @param remaning hoeveel coins er nog moet worden
+     */
+    public void bittrexUpdateOrder(String orderID, double remaning) {
+
+        //timestamp
+        int timeStamp = timeModule.getTimeStamp();
+
+        //exchange
+        String exchange = "bittrex";
+
+        //Update String
+        String updateString = "UPDATE set remaning='" + remaning + "', updateTime='" + timeStamp + "'";
+
+        if (remaning != 0) {
+            updateString += ", status='pending'";
+        } else {
+            updateString += ", status='compleet'";
+        }
+
+        //add WHERE in de sql string
+        updateString += " WHERE exchange='" + exchange + "' AND orderID='" + orderID + "';";
+
+        //run sql command
+        boolean mysqlResponse = mysqlSql.mysqlStament(updateString);
+
+        //kijk op het goed is gegaan
+        if (mysqlResponse == true) {
+            System.out.println("Order " + orderID + " op " + exchange + " is de timestamp bijgewerkt.");
+        } else {
+            System.err.println("Er is een error bij timestamp updaten bij " + exchange + ". Order id is " + orderID);
+        }
+    }
+
+    /**
+     * Update timstamp methoden
+     *
+     * @param orderID het id nummer van de exchange
+     * @param exchange exchange naam
+     */
+    public void updateTimeStamp(String orderID, String exchange) {
+
+        //krijg de timestamp
+        int timeStamp = timeModule.getTimeStamp();
+
+        //mysql string en de response
+        String updateSql = "UPDATE set updateTime='" + timeStamp + "' WHERE orderID='" + orderID + "' AND exchange='" + exchange + "'";
+        boolean mysqlResponse = mysqlSql.mysqlStament(updateSql);
+
+        //kijk op het goed is gegaan
+        if (mysqlResponse == true) {
+            System.out.println("Order " + orderID + " op " + exchange + " is de timestamp bijgewerkt.");
+        } else {
+            System.err.println("Er is een error bij timestamp updaten bij " + exchange + ". Order id is " + orderID);
+        }
     }
 }
